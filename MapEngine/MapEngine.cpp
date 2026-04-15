81abf63e25c2a8212b1d681233672698d405c859
El-sharqawy <elsharqawy2@gmail.com>
2026-04-15 19:11:39 +0200

    Add project files.

---
+++ MapEngine/MapEngine.cpp
@@ -0,0 +1,12 @@
+﻿// MapEngine.cpp : Defines the entry point for the application.
+//
+
+#include "MapEngine.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
